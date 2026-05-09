6e1560e69ad25b0fc1e603761ac6e5dc19346b94
Ryan Wong <ryandpmwong@gmail.com>
2026-05-09 12:34:09 +1000

    Add placeholder people to main

--- main.gd
+++ main.gd
@@ -1,9 +1,11 @@
 extends Node
 
-
+@export var person_scene:  PackedScene
+var money
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	for i in range(5):
+		create_person(100*(i+1), 100*(i+1))
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -13,4 +15,11 @@ func _process(delta: float) -> void:
 
 
 func create_person(x: float, y: float) -> void:
-	pass
+	# Create a new instance of the Person scene.
+	var person = person_scene.instantiate()
+
+	# Set the person's position.
+	person.position = Vector2(x, y)
+
+	# Spawn the person by adding it to the Main scene.
+	add_child(person)
