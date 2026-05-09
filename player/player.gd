c153822c4320f47397be2cec88555eddc4b7a74b
kowasaur <75297283+kowasaur@users.noreply.github.com>
2026-05-09 12:53:02 +1000

    (#8) Fix WASD and don't let player go out of bounds

--- player/player.gd
+++ player/player.gd
@@ -2,16 +2,30 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 
-func _physics_process(delta: float) -> void:
-	var direction := Vector2(
-		Input.get_axis("ui_left", "ui_right"),
-		Input.get_axis("ui_up", "ui_down")
-	)
+var half_size: Vector2
 
+
+func _ready() -> void:
+	var shape := $CollisionShape2D.shape as RectangleShape2D
+	half_size = shape.size / 2
+
+
+func _physics_process(_delta: float) -> void:
+	var direction := Vector2(
+		Input.get_axis("move_left", "move_right"),
+		Input.get_axis("move_up", "move_down")
+	)
+	
 	if direction != Vector2.ZERO:
-		direction = direction.normalized()
-		velocity = direction * SPEED
+		velocity = direction.normalized() * SPEED
 	else:
 		velocity = velocity.move_toward(Vector2.ZERO, SPEED)
-
+	
 	move_and_slide()
+	clamp_to_viewport()
+
+
+func clamp_to_viewport() -> void:
+	var viewport_size := get_viewport_rect().size
+	global_position.x = clamp(global_position.x, half_size.x, viewport_size.x - half_size.x)
+	global_position.y = clamp(global_position.y, half_size.y, viewport_size.y - half_size.y)
